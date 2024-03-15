33b0bd05d09cc4d49223e42d81509f852567c9bf
AdnaNation <azizulislamadnan@gmail.com>
2024-03-15 17:18:29 +0600

    asdfh

--- src/components/Bookmarks/Bookmarks.jsx
+++ src/components/Bookmarks/Bookmarks.jsx
@@ -3,7 +3,7 @@ import Bookmark from '../Bookmark/Bookmark';
 const Bookmarks = ({bookmarks, readingTime}) => {
     return (
         <div className="md: w-1/3 bg-gray-300 ml-4 mt-2 pt-4">
-            <h2>Reading time: {readingTime}</h2>
+            <h2 className='text-4xl text-center border border-blue-400'>Reading time: {readingTime}</h2>
             <h2 className="text-3xl text-center ">Bookmarked Blogs:{bookmarks.length}</h2>
             {
             bookmarks.map(bookmark => <Bookmark key={bookmark.id} bookmark={bookmark}></Bookmark>)
